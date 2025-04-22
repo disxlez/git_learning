34d544d01a1b37a8c7b9e18c02ef4839142e60a7
Isabella Arzate <ig.arzate21@gmail.com>
2025-04-21 20:07:45 -0400

    updated ALLOWED_HOSTS in myproject/settings.py for EC2 access

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e9w=th#!1hgja=#9ys410&1_w-%#su$qt3q5yraz*rjhk5u_pp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Not for production, just dev/testing
 
 
 # Application definition
